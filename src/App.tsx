bf084d4a34d41091ba587624ba0e3ac2b49c2793
rafael <rafael@rss-MacBook-Pro.local>
2023-10-22 18:11:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Hero from "./blocks/Hero";
+import Panel from "./blocks/Panel";
+import Raviga from "./blocks/Raviga";
+import TechLead from "./blocks/TechLead";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Raviga />
+      <Panel />
+      <TechLead />
+    </div>
+  );
+}
+
+export default App;
